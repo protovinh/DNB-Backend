7024b299c957fbb2de32d4a40f64510d07ae409b
protovinh <60383292+protovinh@users.noreply.github.com>
2023-08-05 14:25:00 +0200

    health endpoint

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ const corsOption = {
   optionSuccessStatus: 200,
 };
 
+const port = process.env.PORT || 4000;
+
 app.use(cors(corsOption));
+app.get("/health", (req, res) => {
+  res.status(200).send({ status: "ok" });
+});
 app.use("/", router);
 
-const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log("server is running on port" + port);
 });
